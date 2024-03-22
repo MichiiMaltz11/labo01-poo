602f3c8502697ffd1220c5cf580942161904ed74
00100121@uca.edu.sv <00100121@uca.edu.sv>
2024-03-22 09:31:28 -0600

    Base de las clases de la calculadora

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,53 @@
+
+package calculator;
+import java.util.Scanner;
+
+public class Calculator {
+
+   
+    public static void main(String[] args) 
+    {
+        int opcion;
+        do
+        {
+            Scanner op = new Scanner (System.in);
+        
+            System.out.println("Calculadora");
+            System.out.println("1.Sumar \n2.Restar \n3.Multiplicar \n4.Dividir \n5.Salir");
+            opcion = op.nextInt();
+            switch (opcion)
+            {
+                case 1:
+                    
+                    
+                    
+                    break;
+                case 2:
+                    
+                    
+                    break;
+                case 3:
+                    
+                    
+                    
+                    break;
+                case 4:
+                        
+                       
+                    
+                    break;  
+                default:
+                    break;
+            }
+        }
+        while(opcion != 5);
+           
+        
+        
+        
+        
+        
+        
+    }
+    
+}
