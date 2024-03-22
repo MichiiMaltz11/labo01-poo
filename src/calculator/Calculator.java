8409d6527e408e6ed53d0b8fdd021b04b8f028d1
00100121@uca.edu.sv <00100121@uca.edu.sv>
2024-03-22 10:24:20 -0600

    Calculadora terminada

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,4 +1,3 @@
-
 package calculator;
 import java.util.Scanner;
 
@@ -10,17 +9,22 @@ public class Calculator {
         int opcion;
         do
         {
-            Scanner op = new Scanner (System.in);
+            Scanner sc = new Scanner (System.in);
         
             System.out.println("Calculadora");
             System.out.println("1.Sumar \n2.Restar \n3.Multiplicar \n4.Dividir \n5.Salir");
-            opcion = op.nextInt();
+            System.out.println("Ingresa la opcion: ");
+            opcion = sc.nextInt();
             switch (opcion)
             {
                 case 1:
-                    
-                    
-                    
+                   
+                    System.out.println("Ingrese el primer numero: ");
+                    float sum1 = sc.nextFloat();
+                    System.out.println("Ingrese el segundo numero ");
+                    float sum2 = sc.nextFloat();
+                    System.out.println("La suma es: " + (sum1 + sum2));
+              
                     break;
                 case 2:
                     
